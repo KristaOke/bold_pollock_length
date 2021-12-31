31c48493b63a7f050815bfc7101e095e8f4971bd
KristaOke <kristaoke@gmail.com>
2021-12-31 12:16:45 -0900

    some plots to look at data

--- scripts/load_explor_covar_data.R
+++ scripts/load_explor_covar_data.R
@@ -41,6 +41,8 @@ ggplot(climdat, aes(year, south.sst.ndjfm)) + geom_point()
 
 #this data is all downloaded from EBS ESR report cards available online at 
 # https://apps-afsc.fisheries.noaa.gov/refm/reem/ecoweb/index.php?ID=1
+#contacts are in second row of original data which is skipped here for
+#compatibility with R
 
 #pelagic forager biomass (fish 1000t)
 pel_dat <- read.csv("./data/Pelagic.csv", skip=4, header=TRUE, na.strings="null")
@@ -56,6 +58,32 @@ colnames(bnth_dat)[colnames(bnth_dat) == 'Value'] <- 'benthic_forager_biomass'
 forg_dat <- read.csv("./data/Forage.csv", skip=4, header=TRUE, na.strings="null")
 View(forg_dat)
 colnames(forg_dat)[colnames(forg_dat) == 'Value'] <- 'forage_fish_biomass'
+
+# Motile epifauna biomass (fish and inverts 1000t)
+epif_dat <- read.csv("./data/Epifauna.csv", skip=4, header=TRUE, na.strings="null")
+View(epif_dat)
+colnames(epif_dat)[colnames(epif_dat) == 'Value'] <- 'epifauna_biomass'
+
+
+#Apex predator biomass (fish 1000t)
+#THIS INCLUDES P COD AND ARROWTOOTH
+apex_dat <- read.csv("./data/Apex.csv", skip=4, header=TRUE, na.strings="null")
+View(apex_dat)
+colnames(apex_dat)[colnames(apex_dat) == 'Value'] <- 'apex_pred_biomass'
+
+
+#Euphausiid density (no. m3), 2004-2020
+euph_dat <- read.csv("./data/Euphs.csv", skip=4, header=TRUE, na.strings="null")
+View(euph_dat)
+colnames(euph_dat)[colnames(euph_dat) == 'Value'] <- 'euph_density'
+
+esr_dat <- left_join(pel_dat, bnth_dat, by='Year') %>%
+  left_join(., forg_dat, by='Year') %>%
+  left_join(., epif_dat, by='Year') %>%
+  left_join(., apex_dat, by='Year') %>%
+  left_join(., euph_dat, by='Year') 
+
+View(esr_dat)
 
 #predator abundance data=================
 
@@ -80,4 +108,28 @@ ggplot(mean_wF[which(mean_wF$AGE<16),], aes(YEAR, mean_annual_size_global)) +
   geom_point() + 
   facet_wrap(~AGE, scales="free")
 
+mean_wF_esr <- left_join(mean_wF, esr_dat, by=c("YEAR"="Year"))
 
+ggplot(mean_wF_esr[which(mean_wF_esr$AGE<16),], aes(YEAR, pelagic_forager_biomass)) +
+  geom_point() + 
+  facet_wrap(~AGE, scales="free")
+
+ggplot(mean_wF_esr[which(mean_wF_esr$AGE<16),], aes(YEAR, benthic_forager_biomass)) +
+  geom_point() + 
+  facet_wrap(~AGE, scales="free")
+
+ggplot(mean_wF_esr[which(mean_wF_esr$AGE<16),], aes(YEAR, forage_fish_biomass)) +
+  geom_point() + 
+  facet_wrap(~AGE, scales="free")
+
+ggplot(mean_wF_esr[which(mean_wF_esr$AGE<16),], aes(YEAR, epifauna_biomass)) +
+  geom_point() + 
+  facet_wrap(~AGE, scales="free")
+
+ggplot(mean_wF_esr[which(mean_wF_esr$AGE<16),], aes(YEAR, apex_pred_biomass)) +
+  geom_point() + 
+  facet_wrap(~AGE, scales="free")
+
+ggplot(mean_wF_esr[which(mean_wF_esr$AGE<16),], aes(YEAR, euph_biomass)) +
+  geom_point() + 
+  facet_wrap(~AGE, scales="free")
