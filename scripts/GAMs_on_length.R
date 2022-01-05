3f917616260ba98045353243e72aa4213191f7b5
KristaOke <kristaoke@gmail.com>
2022-01-04 15:24:48 -0900

    model fitting work

--- scripts/GAMs_on_length.R
+++ scripts/GAMs_on_length.R
@@ -75,12 +75,17 @@ ggplot(analysis_dat, aes(south.sst.amj, mean_annual_size_global, col=as.factor(Y
   geom_point() + 
   facet_wrap(~AGE, scales="free")
 
-ggplot(analysis_dat, aes(YEAR, F)) +
+ggplot(analysis_dat, aes(YEAR, mean_annual_F3plus)) +
   geom_point()+ geom_smooth() +facet_wrap(~AGE)
 
-ggplot(analysis_dat, aes(F, mean_annual_size_global, col=as.factor(YEAR))) +
+ggplot(analysis_dat, aes(mean_annual_F3plus, mean_annual_size_global, col=as.factor(YEAR))) +
   geom_point() + 
   facet_wrap(~AGE, scales="free")
+
+
+ggplot(analysis_dat, aes(south.sst.amj, mean_annual_F3plus, col=as.factor(YEAR))) + 
+  geom_point() + facet_wrap(~AGE, scales = "free")
+
 
 #GAMs-----
 
@@ -90,29 +95,29 @@ library(car)
 
 #age 1------
 age1dat <- analysis_dat[which(analysis_dat$AGE==1),]
-mod1 <- gam(mean_annual_size_global ~ s(south.sst.amj, k=4) + s(F) + s(pollock_abun_bil_at_age, k=4) +
-             s(apex_pred_biomass, k=4) + s(epifauna_biomass, k=4) +
-              s(forage_fish_biomass, k=4) + s(benthic_forager_biomass, k=4)+
+mod1 <- gam(mean_annual_size_global ~ s(south.sst.amj, k=4) + s(mean_annual_F3plus) + s(pollock_abun_bil_at_age, k=4) +
+             s(apex_pred_biomass, k=4) + 
+              s(forage_fish_biomass, k=4) + 
               s(pelagic_forager_biomass, k=4), 
             data=age1dat)
 gam.check(mod1)
 plot(mod1) #all look very linear EXCEPT forage fish, pelagic forager
 summary(mod1)
 
-mod1.1 <- gam(mean_annual_size_global ~ south.sst.amj*F + south.sst.amj*pollock_abun_bil_at_age +
-              apex_pred_biomass + epifauna_biomass +
-              s(forage_fish_biomass, k=4) + benthic_forager_biomass +
+mod1.1 <- gam(mean_annual_size_global ~ south.sst.amj*mean_annual_F3plus + south.sst.amj*pollock_abun_bil_at_age +
+              apex_pred_biomass + 
+              s(forage_fish_biomass, k=4) + 
               s(pelagic_forager_biomass, k=4), 
             data=age1dat)
 gam.check(mod1.1)
 plot(mod1.1) 
 summary(mod1.1)
 Anova(mod1.1, type="III")
-anova(mod1.1)
+anova(mod1.1) #nothing significant?
 
-mod1.2 <- gam(mean_annual_size_global ~ south.sst.amj*F + pollock_abun_bil_at_age +
-                apex_pred_biomass + epifauna_biomass +
-                s(forage_fish_biomass, k=4) + benthic_forager_biomass +
+mod1.2 <- gam(mean_annual_size_global ~ south.sst.amj*mean_annual_F3plus + pollock_abun_bil_at_age +
+                apex_pred_biomass + 
+                s(forage_fish_biomass, k=4) + 
                 s(pelagic_forager_biomass, k=4), 
               data=age1dat)
 gam.check(mod1.2)
@@ -122,131 +127,232 @@ Anova(mod1.2, type="III")
 anova(mod1.2)
 
 
+mod1.3 <- gam(mean_annual_size_global ~ south.sst.amj + mean_annual_F3plus + pollock_abun_bil_at_age +
+                apex_pred_biomass + 
+                s(forage_fish_biomass, k=4) + 
+                s(pelagic_forager_biomass, k=4), 
+              data=age1dat)
+gam.check(mod1.3)
+plot(mod1.3) 
+summary(mod1.3)
+Anova(mod1.3, type="III")
+anova(mod1.3)
+
 #age 2------
 age2dat <- analysis_dat[which(analysis_dat$AGE==2),]
-mod2 <- gam(mean_annual_size_global ~ s(south.sst.amj, k=4) + s(F)+ s(pollock_abun_bil_at_age, k=4) +
-              s(apex_pred_biomass, k=4) + s(epifauna_biomass, k=4) +
-              s(forage_fish_biomass, k=4) + s(benthic_forager_biomass, k=4)+
+mod2 <- gam(mean_annual_size_global ~ s(south.sst.amj, k=4) + s(mean_annual_F3plus)+ s(pollock_abun_bil_at_age, k=4) +
+              s(apex_pred_biomass, k=4) + 
+              s(forage_fish_biomass, k=4) + 
               s(pelagic_forager_biomass, k=4), 
             data=age2dat)
-gam.check(mod2) #residuals not great!!
-plot(mod2) #forage fish and pelagic nonlinear, likely also pollock abun
+gam.check(mod2) #residuals not great but not super horrific
+plot(mod2) #forage fish, F3plus, and apex nonlinear, likely also pollock abun
 summary(mod2)
 
+mod2.1 <- gam(mean_annual_size_global ~ south.sst.amj + s(mean_annual_F3plus)+ pollock_abun_bil_at_age +
+              s(apex_pred_biomass, k=4) + 
+              s(forage_fish_biomass, k=4) + 
+              s(pelagic_forager_biomass, k=4), 
+            data=age2dat)
+gam.check(mod2.1) #
+plot(mod2.1) #
+summary(mod2.1)
 
+mod2.x <- gam(mean_annual_size_global ~ south.sst.amj + t2(mean_annual_F3plus, south.sst.amj), 
+              data=age2dat)
+gam.check(mod2.x) #
+plot(mod2.x) #
+summary(mod2.x)
+library(gratia)
+draw(mod2.x, select = 1)
 
 #age 3------
 age3dat <- analysis_dat[which(analysis_dat$AGE==3),]
-mod3 <- gam(mean_annual_size_global ~ s(south.sst.amj, k=4)+ s(F) + s(pollock_abun_bil_at_age, k=4) +
-              s(apex_pred_biomass, k=4) + s(epifauna_biomass, k=4) +
-              s(forage_fish_biomass, k=4) + s(benthic_forager_biomass, k=4)+
+mod3 <- gam(mean_annual_size_global ~ s(south.sst.amj, k=4)+ s(mean_annual_F3plus, k=4) + s(pollock_abun_bil_at_age, k=4) +
+              s(apex_pred_biomass, k=4) + 
+              s(forage_fish_biomass, k=4) + 
               s(pelagic_forager_biomass, k=4), 
             data=age3dat)
 gam.check(mod3) #response v fitted a little odd
-plot(mod3) #all except sst seem nonlinear
+plot(mod3) #only F3plus seems nonlinear but looks a little over fit, trying w k=4
 summary(mod3)
 
+mod3.1 <- gam(mean_annual_size_global ~ s(south.sst.amj, k=4) + s(mean_annual_F3plus, k=4) + pollock_abun_bil_at_age +
+              apex_pred_biomass + 
+              forage_fish_biomass + 
+              pelagic_forager_biomass, 
+            data=age3dat)
+gam.check(mod3.1) #response v fitted a little odd
+plot(mod3.1) #
+summary(mod3.1)
 
+mod3.x <- gam(mean_annual_size_global ~ south.sst.amj + s(mean_annual_F3plus, south.sst.amj), 
+            data=age3dat)
+gam.check(mod3.x) #response v fitted a little odd
+plot(mod3.x) #
+summary(mod3.x)
 
+draw(mod3.x, select=1)
 
 #age 4------
 age4dat <- analysis_dat[which(analysis_dat$AGE==4),]
-mod4 <- gam(mean_annual_size_global ~ s(south.sst.amj, k=4)+ s(F) + s(pollock_abun_bil_at_age, k=4) +
-              s(apex_pred_biomass, k=4) + s(epifauna_biomass, k=4) +
-              s(forage_fish_biomass, k=4) + s(benthic_forager_biomass, k=4)+
+mod4 <- gam(mean_annual_size_global ~ s(south.sst.amj, k=4)+ s(mean_annual_F3plus) + s(pollock_abun_bil_at_age, k=4) +
+              s(apex_pred_biomass, k=4) + 
+              s(forage_fish_biomass, k=4) + 
               s(pelagic_forager_biomass, k=4), 
             data=age4dat)
-gam.check(mod4) #residuals look not great, qq too
+gam.check(mod4) #
 plot(mod4) #
-summary(mod4) #only linear are pelagic and epifauna
+summary(mod4) #nonlinear: sst, f3plus, forage
 
+mod4.1 <- gam(mean_annual_size_global ~ s(south.sst.amj, k=4)+ s(mean_annual_F3plus, k=4) + pollock_abun_bil_at_age +
+              apex_pred_biomass + 
+              s(forage_fish_biomass, k=4) + 
+              pelagic_forager_biomass, 
+            data=age4dat)
+gam.check(mod4.1) #
+plot(mod4.1) #
+summary(mod4.1)
 
+mod4.x <- gam(mean_annual_size_global ~ s(south.sst.amj, k=4)+ s(mean_annual_F3plus, south.sst.amj), 
+              data=age4dat)
+gam.check(mod4.x) #
+plot(mod4.x) #
+summary(mod4.x)
 
+draw(mod4.x, select=2)
 
 #age 5------
 age5dat <- analysis_dat[which(analysis_dat$AGE==5),]
-mod5 <- gam(mean_annual_size_global ~ s(south.sst.amj, k=4)+ s(F) + s(pollock_abun_bil_at_age, k=4) +
-              s(apex_pred_biomass, k=4) + s(epifauna_biomass, k=4) +
-              s(forage_fish_biomass, k=4) + s(benthic_forager_biomass, k=4)+
+mod5 <- gam(mean_annual_size_global ~ s(south.sst.amj, k=4)+ s(mean_annual_F3plus) + s(pollock_abun_bil_at_age, k=4) +
+              s(apex_pred_biomass, k=4) + 
+              s(forage_fish_biomass, k=4) + 
               s(pelagic_forager_biomass, k=4), 
             data=age5dat)
 gam.check(mod5) #residuals not amazing
 plot(mod5) #
 summary(mod5)
 
-
+mod5.1 <- gam(mean_annual_size_global ~ south.sst.amj + s(mean_annual_F3plus) + pollock_abun_bil_at_age +
+              apex_pred_biomass + 
+              s(forage_fish_biomass, k=4) + 
+              pelagic_forager_biomass, 
+            data=age5dat)
+gam.check(mod5.1) #
+plot(mod5.1) #
+summary(mod5.1)
 
 
 #age 6------
 age6dat <- analysis_dat[which(analysis_dat$AGE==6),]
-mod6 <- gam(mean_annual_size_global ~ s(south.sst.amj, k=4)+ s(F) + s(pollock_abun_bil_at_age, k=4) +
-              s(apex_pred_biomass, k=4) + s(epifauna_biomass, k=4) +
-              s(forage_fish_biomass, k=4) + s(benthic_forager_biomass, k=4)+
+mod6 <- gam(mean_annual_size_global ~ s(south.sst.amj, k=4)+ s(mean_annual_F3plus) + s(pollock_abun_bil_at_age, k=4) +
+              s(apex_pred_biomass, k=4) + 
+              s(forage_fish_biomass, k=4) + 
               s(pelagic_forager_biomass, k=4), 
             data=age6dat)
 gam.check(mod6)
-plot(mod6) #sst, apex, epifauna are nonlinear
+plot(mod6) #
 summary(mod6)
+
+mod6.1 <- gam(mean_annual_size_global ~ s(south.sst.amj, k=4)+ s(mean_annual_F3plus) + pollock_abun_bil_at_age +
+              apex_pred_biomass + 
+              s(forage_fish_biomass, k=4) + 
+             pelagic_forager_biomass, 
+            data=age6dat)
+gam.check(mod6.1)
+plot(mod6.1) #
+summary(mod6.1)
 
 
 
 
 #age 7------
 age7dat <- analysis_dat[which(analysis_dat$AGE==7),]
-mod7 <- gam(mean_annual_size_global ~ s(south.sst.amj, k=4)+ s(F) + s(pollock_abun_bil_at_age, k=4) +
-              s(apex_pred_biomass, k=4) + s(epifauna_biomass, k=4) +
-              s(forage_fish_biomass, k=4) + s(benthic_forager_biomass, k=4)+
+mod7 <- gam(mean_annual_size_global ~ s(south.sst.amj, k=4)+ s(mean_annual_F3plus, k=4) + s(pollock_abun_bil_at_age, k=4) +
+              s(apex_pred_biomass, k=4) +
+              s(forage_fish_biomass, k=4) + 
               s(pelagic_forager_biomass, k=4), 
             data=age7dat)
 gam.check(mod7)
-plot(mod7) #none very nonlinear
+plot(mod7) #
 summary(mod7)
+
+mod7.1 <- gam(mean_annual_size_global ~ s(south.sst.amj, k=4) + mean_annual_F3plus + pollock_abun_bil_at_age +
+              s(apex_pred_biomass, k=4) + 
+              s(forage_fish_biomass, k=4) + 
+              pelagic_forager_biomass, 
+            data=age7dat)
+gam.check(mod7.1)
+plot(mod7.1) #
+summary(mod7.1)
 
 
 
 
 #age 8------
 age8dat <- analysis_dat[which(analysis_dat$AGE==8),]
-mod8 <- gam(mean_annual_size_global ~ s(south.sst.amj, k=4)+ s(F) + s(pollock_abun_bil_at_age, k=4) +
-              s(apex_pred_biomass, k=4) + s(epifauna_biomass, k=4) +
-              s(forage_fish_biomass, k=4) + s(benthic_forager_biomass, k=4)+
+mod8 <- gam(mean_annual_size_global ~ s(south.sst.amj, k=4)+ s(mean_annual_F3plus) + s(pollock_abun_bil_at_age, k=4) +
+              s(apex_pred_biomass, k=4) + 
+              s(forage_fish_biomass, k=4) + 
               s(pelagic_forager_biomass, k=4), 
             data=age8dat)
 gam.check(mod8)
 plot(mod8) #none very nonlinear
 summary(mod8) 
 
+mod8.1 <- gam(mean_annual_size_global ~ south.sst.amj + mean_annual_F3plus + pollock_abun_bil_at_age +
+              apex_pred_biomass + 
+              forage_fish_biomass + 
+              pelagic_forager_biomass, 
+            data=age8dat)
+#gam.check(mod8.1)
+#plot(mod8.1) #none very nonlinear
+summary(mod8.1) 
 
 
 
 #age 9------
 age9dat <- analysis_dat[which(analysis_dat$AGE==9),]
-mod9 <- gam(mean_annual_size_global ~ s(south.sst.amj, k=4)+ s(F) + s(pollock_abun_bil_at_age, k=4) +
-              s(apex_pred_biomass, k=4) + s(epifauna_biomass, k=4) +
-              s(forage_fish_biomass, k=4) + s(benthic_forager_biomass, k=4)+
+mod9 <- gam(mean_annual_size_global ~ s(south.sst.amj, k=4)+ s(mean_annual_F3plus) + s(pollock_abun_bil_at_age, k=4) +
+              s(apex_pred_biomass, k=4) + 
+              s(forage_fish_biomass, k=4) + 
               s(pelagic_forager_biomass, k=4), 
             data=age9dat)
 gam.check(mod9) #residuals not great
 plot(mod9) #none are nonlinear at all
 summary(mod9)
 
-
+mod9.1 <- lm(mean_annual_size_global ~ south.sst.amj + mean_annual_F3plus + pollock_abun_bil_at_age +
+              apex_pred_biomass + 
+              forage_fish_biomass + 
+              pelagic_forager_biomass, 
+            data=age9dat)
+# gam.check(mod9) #residuals not great
+# plot(mod9) #none are nonlinear at all
+summary(mod9.1)
 
 
 
 
 #age 10------
 age10dat <- analysis_dat[which(analysis_dat$AGE==10),]
-mod10 <- gam(mean_annual_size_global ~ s(south.sst.amj, k=4)+ s(F) + #s(pollock_abun_bil_at_age, k=4) +
-              s(apex_pred_biomass, k=4) + s(epifauna_biomass, k=4) +
-              s(forage_fish_biomass, k=4) + s(benthic_forager_biomass, k=4)+
+mod10 <- gam(mean_annual_size_global ~ s(south.sst.amj, k=4)+ s(mean_annual_F3plus) + #s(pollock_abun_bil_at_age, k=4) +
+              s(apex_pred_biomass, k=4) + 
+              s(forage_fish_biomass, k=4) + 
               s(pelagic_forager_biomass, k=4), 
             data=age10dat)
-gam.check(mod10)
-plot(mod10) #only epifauna very nonlinear
+gam.check(mod10) #residuals are super bad
+plot(mod10) 
 summary(mod10)
 
-
+mod10.1 <- gam(mean_annual_size_global ~ south.sst.amj + s(mean_annual_F3plus) + #s(pollock_abun_bil_at_age, k=4) +
+               apex_pred_biomass + 
+               s(forage_fish_biomass, k=4) + 
+               s(pelagic_forager_biomass, k=4), 
+             data=age10dat)
+gam.check(mod10.1) #residuals are super bad
+plot(mod10.1) 
+summary(mod10)
 
 
 
