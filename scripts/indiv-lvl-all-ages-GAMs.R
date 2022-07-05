fa2805c37a1bf1873a36c5671c13c9ce6d31d064
KristaOke <kristaoke@gmail.com>
2022-07-05 09:56:19 -0800

    updating based on online documentation trying to deal with this nobs issue

--- scripts/indiv-lvl-all-ages-GAMs.R
+++ scripts/indiv-lvl-all-ages-GAMs.R
@@ -371,5 +371,17 @@ gam.check(glob_mod$gam)
 #saveRDS(glob_mod, file=paste(wd,"/scripts/global_model_2_dredge.rds", sep=""))
 glob_mod <- readRDS(file=paste(wd,"/scripts/global_model_2_dredge.rds", sep=""))
 
-dredge_out <- dredge(surnonlinML, beta="sd")
+#following https://r-forge.r-project.org/scm/viewvc.php/*checkout*/pkg/inst/doc/gamm.pdf?revision=91&root=mumin&pathrev=91
+#we set functions so that dredge/MuMIn know how to handle gamm4 output which is usually a list
+gamm4 <- function(...) structure(c(gamm4::gamm4(...), list(call = match.call())),
+                       class = c("gamm", "list"))
+logLik.gamm <- function(object, ...) logLik(object[[if (is.null(object$lme)) "mer" else "lme"]],  ...)
+ formula.gamm <- function(x, ...) formula(x$gam, ...)
+ nobs.gamm <- function(object, ...) nobs(object$gam, ...)
+ nobs.gam <- function(object, ...) stats:::nobs.glm(object, ...)
+  coeffs.gamm <- function(model) coef(model$gam)
+  getAllTerms.gamm <- function(x, ...) getAllTerms(x$gam)
+  tTable.gamm <- function(model, ...) tTable(model$gam)
+
+dredge_out <- dredge(glob_mod, beta="sd")
 #need to confirm the beta is correct here
